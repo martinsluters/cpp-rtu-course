44d4a6579309c0e84210f45edd5bd02c81ec7aa9
Martins Luters <luters.martins@gmail.com>
2020-08-20 20:25:53 +0300

    4th example

---
+++ 2/basic-math-4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Y=a/(a-b)+1/b
+    float Y,a,b;
+    cout<<"Ievadiet divus skaitļus, kuri nav vienādi:\n";
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+    while ( 0 == b || 0 == (a-b)+1/b ) {
+        cout<<"Dalīt ar 0 nav iespējams. Ievadiet vēlreiz a un b\n";
+        cout<<"a: ";
+        cin>>a;
+        cout<<"b: ";
+        cin>>b;
+    }
+    Y = a/(a-b)+1/b;
+    cout<<"Y: "<<Y<<"\n";
+    return 0;
+}
