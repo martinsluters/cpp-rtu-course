20e81f3fc1112efdc2a49fdac352d27b903551f4
Martins Luters <luters.martins@gmail.com>
2020-08-25 20:25:29 +0300

    Basic data types

---
+++ 3/data-types-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int y = 5/3;
+    double y0 = 5.0/3.0;
+    double y1 = 5.2/2.5;
+
+    cout << "y = " << y << " y0 = " << y0 << " y1 = " << y1;
+
+    return 0;
+}
