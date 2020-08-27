11c54c3f2380f45c68eb26e1377cb3763dd9503c
Martins Luters <luters.martins@gmail.com>
2020-08-27 18:57:29 +0300

    Check if int

---
+++ 4/data-types-4.cpp
@@ -0,0 +1,22 @@
+/**
+ * Simple example of cmath lib usage
+ */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void process_questions() {
+    float a;
+    string answer;
+    cout << "Pārbaudīsim vai ievadītais skaitlis ir vesels skaitlis. Ievadiet jebkuru skaitli: " << endl;
+    cout << "a: ";
+    cin >> a;
+    answer = ( floor( a ) == ceil( a ) ? "Šis ir vesels skaitlis" : "Šis nav vesels skaitlis" );
+    cout << answer;
+}
+
+int main() {
+    process_questions();
+    return 1;
+}
