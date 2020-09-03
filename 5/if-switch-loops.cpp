f0951ad80d1e74ec9cf900c75a76c6b8ba7ab001
Martins Luters <luters.martins@gmail.com>
2020-09-03 18:32:52 +0300

    Switch

---
+++ 5/if-switch-loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Y=sqrt(a-b);
+int main() {
+    unsigned short a;
+    cout << "Ievadiet a (datu tips unsigned short) \n";
+    if( cin >> a ) {
+        cout << "D";
+    }
+    return 1;
+}
