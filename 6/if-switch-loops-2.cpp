f85c8b41ba0333b400aa671a8c2fb134769e1086
Martins Luters <luters.martins@gmail.com>
2020-09-05 13:16:07 +0300

    Improvements

--- 6/if-switch-loops-2.cpp
+++ 6/if-switch-loops-2.cpp
@@ -8,10 +8,20 @@ int main() {
     cout << "Ievadiet velosipeda cenu: ";
     cin >> cena_velosipedam;
 
+    if ( ! cena_velosipedam ) {
+      cout << "Tev paveicās. Riteni uzdāvināja veikalnieks!";
+      return 0;
+    }
+
     cout << "Ievadiet davinātās naudas summu: ";
     cin >> davana;
 
     uzkr_nauda = davana;
+
+    if ( uzkr_nauda >= cena_velosipedam ) {
+      cout << "Tev paveicās. Riteni uzdāvināja vecmāmiņa!";
+      return 0;
+    }
 
     while ( uzkr_nauda < cena_velosipedam ) {
       cout << "Dodamies strādāt. Cik nopelnījāt šodien?: ";
@@ -23,10 +33,17 @@ int main() {
 
     cout << "Var nopirkt riteni! Uzkrātā naudas summa ir €" << uzkr_nauda << "\n";
 
-    cout << "Strādāja " << dienas_stradaja << " dienas\n";
-
     if ( dienas_stradaja ) {
-      cout << "Vidēji darba dienā nopelnīja: €" << round( (visas_darba_dienas_nopelnits / dienas_stradaja) * 100 ) / 100;
+
+      $day_string_v1 = dienas_stradaja > 1 ? "dienas!" : "dienu!";
+      cout << "Strādāja " << dienas_stradaja << " " << $day_string_v1 << "\n";
+
+      if ( dienas_stradaja > 1 ) {
+        cout << "Vidēji darba dienā nopelnīja: €" << round( (visas_darba_dienas_nopelnits / dienas_stradaja) * 100 ) / 100;
+      } else {
+        cout << "Darba dienā nopelnīji: €" << visas_darba_dienas_nopelnits;
+      }
+
     }
 
 
