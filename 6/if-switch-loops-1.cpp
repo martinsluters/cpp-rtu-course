239e0560140ff481170d0439426f568246ea463a
Martins Luters <luters.martins@gmail.com>
2020-09-05 10:44:14 +0300

    Improved spacing

--- 6/if-switch-loops-1.cpp
+++ 6/if-switch-loops-1.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
+
+
     for( int i = 1; i <= 10; i++ ){
         if ( 1 != i ) {
             cout << "\n";
         }
-       for( int j = 1; j <= 10; j++ ){
-           cout << j*i << "\t";
+       for( int j = 1; j <= 10; j++ ) {
+           int multiplied = j*i;
+           cout << multiplied;
+           if ( multiplied >= 10 ) {
+               cout << "  ";
+           } else {
+               cout << "   ";
+           }
+
        }
     }
     return 0;
