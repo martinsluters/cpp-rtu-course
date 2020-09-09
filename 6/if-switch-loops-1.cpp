aaa7a91d4a745d5eb12c597e12157cdf8dad95a9
Martins Luters <luters.martins@gmail.com>
2020-09-09 09:46:28 +0300

    Multiplication table improved

--- 6/if-switch-loops-1.cpp
+++ 6/if-switch-loops-1.cpp
@@ -1,14 +1,26 @@
+/**
+ * Multiplication table in high wind speeds
+ */
+
 #include <iostream>
 using namespace std;
+
+string print_spaces( int no_spaces ) {
+  string spaces = "";
+  for ( no_spaces; no_spaces > 0; no_spaces-- ) {
+    spaces += " ";
+  }
+  return spaces;
+}
+
 int main() {
-
-
+    int multiplied;
     for( int i = 1; i <= 10; i++ ){
-        if ( 1 != i ) {
-            cout << "\n";
+        if ( 1 < i ) {
+            cout << "\n" << print_spaces( i-1 );
         }
        for( int j = 1; j <= 10; j++ ) {
-           int multiplied = j*i;
+           multiplied = j*i;
            cout << multiplied;
            if ( multiplied >= 10 ) {
                cout << "  ";
