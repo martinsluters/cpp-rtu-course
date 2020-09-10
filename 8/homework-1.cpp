cc02e449f7bd0422fddcc68e351b234a91831ba6
Martins Luters <luters.martins@gmail.com>
2020-09-10 19:02:02 +0300

    Homework 1

---
+++ 8/homework-1.cpp
@@ -0,0 +1,30 @@
+/**
+ * [LV]
+ * Izveidot programmu, kur lietotājs ievada skaitli un vārdu, programma izvada vārdu
+ * tik  reizes, kāds skaitlis ir ievadīts.
+ *
+ * [ENG]
+ * Create software that allows to enter your name and number X. The software should output your name
+ * X number of times.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	string name = "";
+
+	cout  << "Please enter your name: ";
+	cin >> name;
+
+	cout << "Please enter number: ";
+	cin >> number;
+	number = abs( number );
+
+	for ( int i = 0; i < number; i++ ) {
+		cout << i << ". " << name << "\n";
+	}
+
+	return 0;
+}
