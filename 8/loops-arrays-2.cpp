3194d0309d26fd4218d1c6bda4d794244afc40ab
Martins Luters <luters.martins@gmail.com>
2020-09-11 10:16:26 +0300

    Change to LAT

--- 8/loops-arrays-2.cpp
+++ 8/loops-arrays-2.cpp
@@ -5,7 +5,7 @@ int main() {
 	int month_no = 0;
 	string months[12] = {"Janvāris", "Februāris", "Marts", "Aprīlis", "Maijs", "Jūnijs", "Jūlijs", "Augusts", "Septembris", "Oktobris", "Novembris", "Decembris"};
 	do {
-		cout << "Please enter month number (1-12): ";
+		cout << "Ievadiet mēnenesi ar skaitli (1-12): ";
 		cin >> month_no;
 	} while ( month_no < 1 || month_no > 12 );
 
