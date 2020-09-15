cb6e1dbcd311206d6cbe342245a2146386a8f301
Martins Luters <luters.martins@gmail.com>
2020-09-15 18:38:05 +0300

    Descending bubble sort

---
+++ 9/loops-arrays.cpp
@@ -0,0 +1,45 @@
+/**
+ * [LV]
+ * masīvā ir 10 skaitļi, kurus norāda programmētājs,
+ * sakārtot masīvu dilstošā secībā un noteikt cik ir minimālās vērtības.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	srand( time( NULL ) );
+
+	int arr_len = 10, array[ arr_len ] = {12, 122, -121, 121, 146, 9, 0, 24, -121, 762}, i, N, curr_min_val, count_min_values = 1;
+
+	for ( N = arr_len-1; N > 0; N-- ) {
+		for ( i = 0; i < N; i++ ) {
+			int pal = array[i];
+			if ( array[i] < array[i + 1] ) {
+				array[i] = array[i + 1 ];
+				array[i + 1] = pal;
+			}
+		}
+	}
+
+	curr_min_val = array[arr_len-1];
+
+	for ( i = arr_len-2; i >= 0; i-- ) {
+		if ( curr_min_val == array[i] ) {
+			count_min_values++;
+		}
+	}
+
+	cout << "\n\n";
+
+	for ( i = 0; i < arr_len; i++ ) {
+		cout << array[i] << "\t";
+	}
+
+	cout << "\n\n";
+
+	cout << "Minimālo vērtību skaits: " << count_min_values;
+
+	return 0;
+}
