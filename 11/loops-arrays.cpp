56785b6e9924243b1fd39eb436003b4dc8beaebc
Martins Luters <luters.martins@gmail.com>
2020-09-22 17:59:08 +0300

    2d arr

---
+++ 11/loops-arrays.cpp
@@ -0,0 +1,28 @@
+/**
+ * [LV]
+ * Aizpildīt divdimensija masīvu ar gadījuma
+ * skaitļiem no -5 līdz 5 un aprēķināt masīva elementu summu.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand( time( NULL ) );
+	int total_sum = 0, arr_len = 10, array[ arr_len ][ arr_len ], i, j;
+
+	for ( i = 0; i < 10; i++ ) {
+		for ( j = 0; j < 10; j++ ) {
+			array[i][j] = rand() % 11 - 5;
+			cout << array[i][j] << "\t";
+			total_sum += array[i][j];
+		}
+		cout << "\n";
+	}
+
+	cout << "\n\n";
+
+	cout << "Kopējā 2D masīva elementu summa ir: " << total_sum;
+
+	return 0;
+}
