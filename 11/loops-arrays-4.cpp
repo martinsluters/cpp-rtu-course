5cfb0ad0cb5a51e8fdcb25074c330ddc609c4495
Martins Luters <luters.martins@gmail.com>
2020-09-22 19:51:31 +0300

    Array ORDERING

--- 11/loops-arrays-4.cpp
+++ 11/loops-arrays-4.cpp
@@ -1,7 +1,6 @@
 /**
  * [LV]
- * Sakārtot divdimensija masīva vērtības dilstošā secībā pa kolonām
- * un aprēķināt katras kolonas summu un vidējo.
+ * Sakārtot visu divdimensiju masīvu augošā secībā pa rindām un kolonām
  */
 
 #include <iostream>
@@ -11,6 +10,7 @@ int main() {
 	srand( time( NULL ) );
 	int total_sum_col, arr_len = 4, array[ arr_len ][ arr_len ], i, j, N, pal;
 
+	cout << "ORIĢINĀLAIS MASĪVS" << "\n\n";
     for ( i = 0; i < arr_len; i++ ) {
 		for ( j = 0; j < arr_len; j++ ) {
 		    array[i][j] = rand() % 5 + 1;
@@ -21,18 +21,19 @@ int main() {
 
 	cout << "\n\n\n";
 
-	for ( j = 0; j < arr_len; j++ ) {
+	for ( i = 0; i < arr_len; i++ ) {
 		for( N = arr_len-1; N > 0; N-- ) {
-			for ( i = 0; i < N; i++ ) {
+			for ( j = 0; j < N; j++ ) {
 				pal = array[i][j];
-				if ( array[i][j] < array[i+1][j] ) {
-					array[i][j] = array[i+1][j];
-					array[i+1][j] = pal;
+				if ( array[i][j] > array[i][j+1] ) {
+					array[i][j] = array[i][j+1];
+					array[i][j+1] = pal;
 				}
 			}
 		}
 	}
 
+	cout << "MASĪVS SAKĀRTOTS AUGOŠĀ SECĪBĀ PA RINDĀM" << "\n\n";
 	for ( i = 0; i < arr_len; i++ ) {
 		for ( j = 0; j < arr_len; j++ ) {
 			cout << array[i][j] << "\t";
@@ -42,15 +43,28 @@ int main() {
 
 	cout << "\n\n\n";
 
+
 	for ( j = 0; j < arr_len; j++ ) {
-	    total_sum_col = 0;
-		for ( i = 0; i < arr_len; i++ ) {
-			total_sum_col += array[i][j];
+		for( N = arr_len-1; N > 0; N-- ) {
+			for ( i = 0; i < N; i++ ) {
+				pal = array[i][j];
+				if ( array[i][j] > array[i+1][j] ) {
+					array[i][j] = array[i+1][j];
+					array[i+1][j] = pal;
+				}
+			}
 		}
-	    cout << j+1 << ". kolonnas summa: " << total_sum_col << "; ";
-		cout << j+1 << ". kolonnas vidējā vērtība: " << total_sum_col / ( arr_len * 1.0 ) << ";";
+	}
+
+	cout << "MASĪVS SAKĀRTOTS AUGOŠĀ SECĪBĀ PA KOLONĀM PĒC SAKĀRTOŠANAS AUGOŠĀ SECĪBĀ PA RINDĀM" << "\n\n";
+	for ( i = 0; i < arr_len; i++ ) {
+		for ( j = 0; j < arr_len; j++ ) {
+			cout << array[i][j] << "\t";
+		}
 		cout << "\n";
 	}
+
+	cout << "\n\n\n";
 
 	return 0;
 }
