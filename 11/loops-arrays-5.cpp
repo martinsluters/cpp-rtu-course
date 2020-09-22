dcf6c7e85e93f6ca5e89ae37b4e4ff27762de96b
Martins Luters <luters.martins@gmail.com>
2020-09-22 19:43:00 +0300

    Inverse array

---
+++ 11/loops-arrays-5.cpp
@@ -0,0 +1,35 @@
+/**
+ * [LV]
+ * Izveidot divdimensija masīvu, kas aizpildīti ar gadījuma skaitļiem.
+ * Izveidot jaunu masīvu (transporēto), kas ir apgriezta pirmajam
+ * (rindas ar kolonām apmainīta vietām). (Par transponēto matricu sauc matricu AT,
+ * kuru iegūst no dotās matricas, rindas elementus mainot vietām ar atbilstošās kolonnas elementiem.)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand( time( NULL ) );
+	int arr_len = 5, array[ arr_len ][ arr_len ], new_array[ arr_len ][ arr_len ], i, j, pal_arr[ arr_len ];
+
+    for ( i = 0; i < arr_len; i++ ) {
+		for ( j = 0; j < arr_len; j++ ) {
+		    array[i][j] = rand() % 5 + 1;
+		    new_array[j][i] = array[i][j];
+			cout << array[i][j] << "  ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\n\n\n";
+
+	for ( i = 0; i < arr_len; i++ ) {
+		for ( j = 0; j < arr_len; j++ ) {
+			cout << new_array[i][j] << "  ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
